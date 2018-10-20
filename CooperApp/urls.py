597406f20aee7cf4bfd2cb1bbcabce7397011f49
LautaroNavarro <navarro_lautaro@hotmail.com>
2018-10-20 19:33:16 -0300

    Added mapping objects from api

--- CooperApp/urls.py
+++ CooperApp/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
-from CooperApp.views import index
+from CooperApp.views import Index
 
 urlpatterns = [
-    url(r'^index/', index),
+    url(r'^index/', Index.as_view()),
 ]
