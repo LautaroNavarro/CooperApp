3f49f8f65fd015bf6ed34145b6102cc955143933
LautaroNavarro <navarro_lautaro@hotmail.com>
2018-10-21 16:23:06 -0300

    De todito

--- CooperApp/urls.py
+++ CooperApp/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
-from CooperApp.views import Index, LandingPage
+from CooperApp.views import Index, LandingPage, create_user_of_news
 
 urlpatterns = [
     url(r'^index/', Index.as_view(), name="index"),
+    url(r'^save_news/', create_user_of_news, name='save_news'),
     url(r'^', LandingPage, name='landing'),
 ]
--- CooperApp/views.py
+++ CooperApp/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
 from CooperApp.utils import get_api_launches
+from CooperApp.models import *
+from django.core.urlresolvers import reverse
+from django.shortcuts import redirect
 # Create your views here.
 
 
@@ -13,5 +16,17 @@ class Index(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super(Index, self).get_context_data(**kwargs)
-        context["api_launches"] = get_api_launches("2018-10-20", "2035-10-20")
+        if self.request.GET.get('start_date') and self.request.GET.get('end_date'):
+        	start_date = self.request.GET.get('start_date')
+        	end_date = self.request.GET.get('end_date')
+        else:
+        	start_date = ""
+        	end_date = ""
+        context["api_launches"] = get_api_launches(start_date, end_date)
         return context
+
+
+def create_user_of_news(request):
+	email = request.POST.get("email")
+	NewsLetterSubscribed.objects.create(email=email)
+	return redirect(reverse('index'))
